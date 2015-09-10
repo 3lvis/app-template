a587618ee8ead3aa402d20cfa00f5afb16bfebe9
Elvis Nuñez <elvisnunez@me.com>
2015-09-10 13:46:35 +0200

    Compile with Xcode 7 and Swift 2 GM

--- App/Source/RootView.swift
+++ App/Source/RootView.swift
@@ -7,7 +7,7 @@ class RootView: UIView {
         backgroundColor = .redColor()
     }
 
-    required init(coder aDecoder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
