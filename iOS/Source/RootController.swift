98bb1342bc96ffe6a7de63f37446f6d39d72bd0e
Elvis Nuñez <elvisnunez@me.com>
2015-10-13 16:54:38 +0200

    Update project template

--- iOS/Source/RootController.swift
+++ iOS/Source/RootController.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class RootController: UIViewController {
     override func loadView() {
-        let view = RootView(frame: appFrame())
+        let view = RootView(frame: UIScreen.mainScreen().bounds)
         self.view = view
     }
 
