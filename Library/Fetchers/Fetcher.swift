4a9a1ac81eb40f79df6ea783d74f5ca7f9561566
Elvis Nuñez <elvisnunez@me.com>
2016-05-21 21:44:01 +0200

    Remove unused from Fetcher

--- Library/Fetchers/Fetcher.swift
+++ Library/Fetchers/Fetcher.swift
@@ -14,10 +14,6 @@ public class Fetcher {
         self.networking = Networking(baseURL: baseURL)
     }
 
-    public func persistWithCompletion(completion: () -> ()) {
-        data.persistWithCompletion(completion)
-    }
-
     public func someResource(completion: (error: NSError?) -> ()) {
         self.networking.GET("/someResource") { JSON, error in
             if let JSON = JSON as? [[String : AnyObject]] {
@@ -28,15 +24,5 @@ public class Fetcher {
                 completion(error: error)
             }
         }
-    }
-}
-
-extension Fetcher {
-    public func dataSource(tableView: UITableView, cellIdentifier: String, fetchRequest: NSFetchRequest, sectionName: String? = nil, configuration: (cell: UITableViewCell, item: NSManagedObject, indexPath: NSIndexPath) -> ()) -> DATASource {
-        return DATASource(tableView: tableView, cellIdentifier: cellIdentifier, fetchRequest: fetchRequest, mainContext: self.data.mainContext, sectionName: sectionName, configuration: configuration)
-    }
-
-    public func dataSource(collectionView: UICollectionView, cellIdentifier: String, fetchRequest: NSFetchRequest, mainContext: NSManagedObjectContext, sectionName: String? = nil, configuration: (cell: UICollectionViewCell, item: NSManagedObject, indexPath: NSIndexPath) -> ()) -> DATASource {
-        return DATASource(collectionView: collectionView, cellIdentifier: cellIdentifier, fetchRequest: fetchRequest, mainContext: self.data.mainContext, sectionName: sectionName, configuration: configuration)
     }
 }
